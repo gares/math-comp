2d36954f89ca15712d60c71038aa42ead86f12a8
Cyril Cohen <cohen@crans.org>
2020-07-16 10:09:32 +0200

    Working nix-shell

---
+++ .nix/config.nix
@@ -0,0 +1,18 @@
+{
+  format = "1.0.0";
+  coq-attribute = "mathcomp-single";
+  pname = "mathcomp";
+  namespace = "mathcomp";
+  realpath = "mathcomp";
+  select = "coq-8.13";
+  inputs."coq-8.13".coqPackages = {
+    coq.override.version = "8.13";
+    hierarchy-builder.override.version = "master";
+    # /home/cyril/git/hierarchy-builder/master;
+  };
+  inputs."coq-8.12".coqPackages = {
+    coq.override.version = "8.12";
+    hierarchy-builder.override.version = "master";
+    # /home/cyril/git/hierarchy-builder/master;
+  };
+}
