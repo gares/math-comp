a4b26299d2a44323019b7ffb1ba4fd92f2a4b81c
Cyril Cohen <cohen@crans.org>
2021-02-18 16:16:31 +0100

    fix remote

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@
 { config ? {}, withEmacs ? false, print-env ? false, do-nothing ? false,
   update-nixpkgs ? false, ci ? false, ci-step ? null, inNixShell ? null
 }@args:
-# let src = fetchGit "https://github.com/coq-community/nix-toolbox.git"; in
-let src = /home/cyril/git/nix-toolbox/master; in
+let src = fetchGit "https://github.com/coq-community/nix-toolbox.git"; in
 (import "${src}/auto-config.nix" ./. args).nix-auto
 
